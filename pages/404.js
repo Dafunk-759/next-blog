108ee0c2cd2fbac8a8c3d3d1229490a20931d82e
dafunk <759974271@qq.com>
2022-02-27 15:19:00 +0800

    change 404 page text.

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,9 @@ import utilStyles from "../styles/utils.module.css"
 export default function Custom404() {
   return (
     <Layout>
-      <h1 className={utilStyles.heading2Xl}>404 - Page Not Found</h1>
+      <h1 className={utilStyles.heading2Xl}>
+        Page Not Found.
+      </h1>
     </Layout>
   )
 }
